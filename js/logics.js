fd6817d0029adf8177a7bc1b30ae1f7879a1e3e8
dekellevy93 <dekelle@post.bgu.ac.il>
2020-05-02 18:20:52 +0300

    added random color and fixed the arrows

--- js/logics.js
+++ js/logics.js
@@ -135,6 +135,15 @@ function randomizeSettings(){
         
 }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  
 
 function scrollto(div) {
     document.getElementById("register").style.display=("none");
